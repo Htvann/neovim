b179ff1133ab99af16c697229b86f808917bb7a4
Htvann <cv.hothanhvan@gmail.com>
2023-07-29 19:51:01 +0700

    vim bookmarks

--- lua/josean/plugins-setup.lua
+++ lua/josean/plugins-setup.lua
@@ -59,6 +59,10 @@ return packer.startup(function(use)
   use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
   use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
+  --bookmarks
+  use({ "MattesGroeger/vim-bookmarks" })
+  use({ "tom-anders/telescope-vim-bookmarks.nvim" })
+
   -- autocompletion
   use("hrsh7th/nvim-cmp") -- completion plugin
   use("hrsh7th/cmp-buffer") -- source for text in buffer

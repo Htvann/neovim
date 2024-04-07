cacc4b63431f7b90efc266435cdae78f3a226a41
vawn <vawn@MacBook-Pro-cua-vawn.local>
2024-04-08 02:00:41 +0700

    Update feature edit

--- lua/josean/plugins/nvim-cmp.lua
+++ lua/josean/plugins/nvim-cmp.lua
@@ -63,9 +63,9 @@ cmp.setup({
   -- sources for autocompletion
   sources = cmp.config.sources({
     { name = "nvim_lsp" }, -- lsp
-    -- { name = "luasnip" }, -- snippets
+    { name = "luasnip" }, -- snippets
     { name = "buffer" }, -- text within current buffer
-    -- { name = "path" }, -- file system paths
+    { name = "path" }, -- file system paths
   }),
   -- configure lspkind for vs-code like icons
   formatting = {

87954aa7772be63ac9a953f11b5024922a4ec1db
Hồ Thanh Văn <hothanhvawn@MacBook-Pro-cua-Ho.local>
2025-08-20 01:08:38 +0700

    document

--- lua/josean/plugins/colorscheme.lua
+++ lua/josean/plugins/colorscheme.lua
@@ -1,55 +1,77 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    -- local transparent = false -- set to true if you would like to enable transparency
-    --
-    -- local bg = "#011628"
-    -- local bg_dark = "#011423"
-    -- local bg_highlight = "#143652"
-    -- local bg_search = "#0A64AC"
-    -- local bg_visual = "#275378"
-    -- local fg = "#CBE0F0"
-    -- local fg_dark = "#B4D0E9"
-    -- local fg_gutter = "#627E97"
-    -- local border = "#547998"
 
-    require("tokyonight").setup({
-      -- style = "night",
-      -- transparent = transparent,
-      -- styles = {
-      --   sidebars = transparent and "transparent" or "dark",
-      --   floats = transparent and "transparent" or "dark",
-      -- },
-      -- on_colors = function(colors)
-      --   colors.bg = bg
-      --   colors.bg_dark = transparent and colors.none or bg_dark
-      --   colors.bg_float = transparent and colors.none or bg_dark
-      --   colors.bg_highlight = bg_highlight
-      --   colors.bg_popup = bg_dark
-      --   colors.bg_search = bg_search
-      --   colors.bg_sidebar = transparent and colors.none or bg_dark
-      --   colors.bg_statusline = transparent and colors.none or bg_dark
-      --   colors.bg_visual = bg_visual
-      --   colors.border = border
-      --   colors.fg = fg
-      --   colors.fg_dark = fg_dark
-      --   colors.fg_float = fg
-      --   colors.fg_gutter = fg_gutter
-      --   colors.fg_sidebar = fg_dark
-      -- end,
-    })
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({})
+      vim.cmd("colorscheme catppuccin")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({})
+      -- vim.cmd("colorscheme tokyonight")
+    end,
+  },
 
-    vim.cmd("colorscheme tokyonight")
-  end,
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    ---@type solarized.config
+    opts = {},
+    config = function(_, opts)
+      vim.o.termguicolors = true
+      vim.o.background = "dark"
+      require("solarized").setup(opts)
+      -- vim.cmd.colorscheme("solarized")
+    end,
+  },
+
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function(_, opts)
+      require("solarized-osaka").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
+      })
+      -- vim.cmd("colorscheme solarized-osaka")
+    end,
+  },
 }
-
--- return {
---   "catppuccin/nvim",
---   name = "catppuccin",
---   priority = 1000,
---   config = function()
---     local transparent = false -- set to true if you would like to enable transparency
---     vim.cmd("colorscheme catppuccin")
---   end,
--- }
--- lua/josean/plugins/lsp/lspconfig.lua
+++ lua/josean/plugins/lsp/lspconfig.lua
@@ -8,7 +8,6 @@ return {
   },
   config = function()
     -- NOTE: LSP Keybinds
-
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(ev)
